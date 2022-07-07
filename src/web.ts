2131b98e9482d93b0c9428dc0033b6850e85aa78
Fernando <f.mendoza.galvez@gmail.com>
2022-07-07 12:50:24 -0500

    add github issue for plugin_name change

--- src/web.ts
+++ src/web.ts
@@ -68,7 +68,8 @@ export class GoogleAuthWeb extends WebPlugin implements GoogleAuthPlugin {
 
   platformJsLoaded() {
     gapi.load('auth2', () => {
-      const clientConfig: gapi.auth2.ClientConfig = {
+      // https://github.com/CodetrixStudio/CapacitorGoogleAuth/issues/202#issuecomment-1147393785
+      const clientConfig: gapi.auth2.ClientConfig & { plugin_name: string } = {
         client_id: this.options.clientId,
         plugin_name: 'FmendozaCapacitorGoogleAuth',
       };
